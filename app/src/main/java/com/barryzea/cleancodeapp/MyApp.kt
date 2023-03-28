5985944f5d59294080ebb40dc5f32d2f82b3e7af
hall9zeha <vmwaretars@gmail.com>
2023-03-28 11:15:11 -0500

    added: libraries for the  project

---
+++ app/src/main/java/com/barryzea/cleancodeapp/MyApp.kt
@@ -0,0 +1,14 @@
+package com.barryzea.cleancodeapp
+
+import android.app.Application
+
+/****
+ * Project CleanCodeApp
+ * Created by Barry Zea H. on 28/3/23.
+ * Copyright (c)  All rights reserved.
+ ***/
+class MyApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/barryzea/cleancodeapp/ui/MainActivity.kt
+++ app/src/main/java/com/barryzea/cleancodeapp/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package com.barryzea.cleancodeapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.barryzea.cleancodeapp.R
+import com.barryzea.cleancodeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
     }
 }
