c8410fa778e4385d0fb4d5a90e060835f5be29b5
hall9zeha <vmwaretars@gmail.com>
2023-03-28 15:40:49 -0500

    added: librería de dagger hilt agregada

--- app/src/main/java/com/barryzea/cleancodeapp/MyApp.kt
+++ app/src/main/java/com/barryzea/cleancodeapp/MyApp.kt
@@ -1,12 +1,14 @@
 package com.barryzea.cleancodeapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /****
  * Project CleanCodeApp
  * Created by Barry Zea H. on 28/3/23.
  * Copyright (c)  All rights reserved.
  ***/
+@HiltAndroidApp
 class MyApp:Application() {
     override fun onCreate() {
         super.onCreate()
